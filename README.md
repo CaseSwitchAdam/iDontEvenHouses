8e71277f9f8c4505ec6fda5590b1326e4c711f91
Lucas Hart <lukke100@gmail.com>
2013-05-04 19:50:58 -0400

    Created the main method.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 ##I Don't Even Houses
 We don't really know where we are going with this. Something about an RPG and houses.
+
+----------------------------------------------------------
+Copyright (C) 2013 Lucas Hart and Adam McCarthy
+
+This file is part of iDontEvenHouses
+
+iDontEvenHouses is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+iDontEvenHouses is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with iDontEvenHouses.  If not, see <http://www.gnu.org/licenses/>.
---
+++ src/com/lukke100/iDontEvenHouses/Plugin.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2013 Lucas Hart and Adam McCarthy
+ * 
+ * This file is part of iDontEvenHouses
+ * 
+ * iDontEvenHouses is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * iDontEvenHouses is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with iDontEvenHouses.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.lukke100.iDontEvenHouses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author lukke
+ *
+ */
+public class Plugin extends JavaPlugin {
+	
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
+	 */
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.java.JavaPlugin#onDisable()
+	 */
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+}
